b6d30612684e09f9cee0f5fced110437b930ff0e
idevnerds-Arslan <arslan@idevnerds.com>
2022-04-27 10:54:12 +0500

    add activity loader integrate api for get agency and tourist count integrate api for user detail

--- src/components/Admin Component/Users/index.js
+++ src/components/Admin Component/Users/index.js
@@ -1,5 +1,5 @@
-import React , {useState} from 'react';
-import './user.css'
+import React, { useState, useEffect } from "react";
+import "./user.css";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,25 +7,36 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import {User} from "../mockdata";
 import { DeleteOutlined, EditOutlined } from "@mui/icons-material";
 import SearchBar from "material-ui-search-bar";
-
-
+import { GET } from "../../../services/httpClient";
+import ActivityLoader from "../../ActivityLoader/index";
+import CancelIcon from "@mui/icons-material/Cancel";
+import VerifiedIcon from "@mui/icons-material/Verified";
 const UserDetail = () => {
-
-  const [rows, setRows] = useState(User);
+  const [rows, setRows] = useState([]);
+  const [record, setRecord] = useState([]);
   const [searched, setSearched] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const requestSearch = (searchedVal) => {
-    const filteredRows = User.filter((row) => {
-      return Object.keys(row).some((username) =>
-        row.username.toLowerCase().includes(searchedVal) ||
-          row.username.toUpperCase().includes(searchedVal)
-
-      );
+  useEffect(() => {
+    getAllActiveUsers();
+  }, []);
+  async function getAllActiveUsers() {
+    let res = await GET("/user/", { params: { isDeleted: false } });
+    if (res) {
+      setRows(res);
+      setIsLoading(false);
     }
-    );
+  }
+  const requestSearch = (searchedVal) => {
+    const filteredRows = rows.filter((row) => {
+      return Object.keys(row).some(
+        (firstName) =>
+          row.firstName.toLowerCase().includes(searchedVal) ||
+          row.firstName.toUpperCase().includes(searchedVal)
+      );
+    });
     setRows(filteredRows);
   };
 
@@ -36,54 +47,62 @@ const UserDetail = () => {
 
   return (
     <>
-    <div className='home'>
-    <div className='listContainer'>
-        <SearchBar
-          value={searched}
-        className='searchbar'
-          onChange={(searchVal) => requestSearch(searchVal)}
-          onCancelSearch={() => cancelSearch()}
-        />
+      <div className="home">
+        <div className="listContainer">
+          <SearchBar
+            value={searched}
+            className="searchbar"
+            onChange={(searchVal) => requestSearch(searchVal)}
+            onCancelSearch={() => cancelSearch()}
+          />
 
-    <TableContainer component={Paper} 
-    className="usertable"
-    >
-      <Table sx={{ minWidth: 650 }}aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>User Name</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow>
-              <TableCell>{row.id}</TableCell>
-              <TableCell className="tableCell" key = {row.id}>{row.username}</TableCell>
-              <TableCell>
-                    <button
-                      className="editbtn"
-                      title='Edit'
-                    >
-                      <EditOutlined />
-                    </button>
-                    <button
-                      className="deletebtn"
-                      title="Delete"
-                    >
-                      <DeleteOutlined />
-                    </button>
-                  </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-    </div>
+          <TableContainer component={Paper} className="usertable">
+            {isLoading && <ActivityLoader />}
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Verify</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {!isLoading &&
+                  rows.map((row) => (
+                    <TableRow key={row.id}>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell className="tableCell">
+                        {row.firstName}
+                      </TableCell>
+                      <TableCell className="tableCell">
+                        {row.lastName}
+                      </TableCell>
+                      <TableCell className="tableCell">
+                        {row.isVerified == "email" ? (
+                          <VerifiedIcon sx={{ color: "#fb9e00" }} />
+                        ) : (
+                          <CancelIcon sx={{ color: "red" }} />
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        <button className="editbtn" title="Edit">
+                          <EditOutlined />
+                        </button>
+                        <button className="deletebtn" title="Delete">
+                          <DeleteOutlined />
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
     </>
   );
 };
 
-export default UserDetail; 
+export default UserDetail;
--- src/components/Admin Component/feactureinfo/index.js
+++ src/components/Admin Component/feactureinfo/index.js
@@ -1,19 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./feactureinfo.css";
+import { GET } from "../../../services/httpClient";
+import RingLoader from "react-spinners/RingLoader";
+import CountUp from "react-countup";
+import { Box } from "@mui/material";
 
 const FeaturedInfo = () => {
+  const [tCount, setTCount] = useState("");
+  const [aCount, setACount] = useState("");
+  let [color, setColor] = useState("#fb9e00");
+  const [progressTourist, setProgressTourist] = useState(false);
+  const [progressAgency, setProgressAgency] = useState(false);
+
+  useEffect(() => {
+    getTouristCount();
+    getAgencyCount();
+  }, []);
+
+  async function getTouristCount() {
+    let res = await GET("/user/touristCount");
+    setTimeout(() => {
+      setProgressTourist(true);
+      setTCount(res);
+    }, 2500);
+  }
+  async function getAgencyCount() {
+    let res = await GET("/user/agencyCount");
+    setTimeout(() => {
+      setProgressAgency(true);
+      setACount(res);
+    }, 3500);
+  }
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Tourist</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoney">
+            {progressTourist ? (
+              <CountUp end={tCount} duration={1.5} />
+            ) : (
+              <Box
+                style={{
+                  height: "100%",
+                  width: "100",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <RingLoader color={color} loading={!progressTourist} />
+              </Box>
+            )}
+          </span>
         </div>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">Agencies</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoney">
+            {progressAgency ? (
+              <CountUp end={aCount} duration={1} />
+            ) : (
+              <Box
+                style={{
+                  height: "100%",
+                  width: "100",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <RingLoader color={color} loading={!progressAgency} />
+              </Box>
+            )}
+          </span>
         </div>
       </div>
       <div className="featuredItem">
