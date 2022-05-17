99ddb784ffdbcad0242fde4d53eacad4b53f8625
idevnerds-Arslan <arslan@idevnerds.com>
2022-05-17 20:27:24 +0500

    inteprate get api in agency tab

--- src/components/Admin Component/Agency/index.js
+++ src/components/Admin Component/Agency/index.js
@@ -1,5 +1,5 @@
-import React , {useState} from 'react';
-import './agency.css'
+import React, { useState, useEffect } from "react";
+import "./agency.css";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,83 +7,124 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import {AgencyName} from "../mockdata";
+import { AgencyName } from "../mockdata";
 import { DeleteOutlined, EditOutlined } from "@mui/icons-material";
 import SearchBar from "material-ui-search-bar";
-
+import { GET } from "../../../services/httpClient";
+import ActivityLoader from "../../ActivityLoader/index";
+import CancelIcon from "@mui/icons-material/Cancel";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import Dialog from "../../Dialog/index";
 
 const AgencyDetail = () => {
-
-  const [rows, setRows] = useState(AgencyName);
+  const [rows, setRows] = useState([]);
   const [searched, setSearched] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getAllActiveUsers();
+  }, []);
+  async function getAllActiveUsers() {
+    let res = await GET("/admin/agency", { params: { isDeleted: false } });
+    if (res) {
+      setRows(res);
+      setIsLoading(false);
+    }
+  }
 
   const requestSearch = (searchedVal) => {
-    const filteredRows = AgencyName.filter((row) => {
-      return Object.keys(row).some((agencyname) =>
-        row.agencyname.toLowerCase().includes(searchedVal) ||
-          row.agencyname.toUpperCase().includes(searchedVal)
-
+    if (!searchedVal) getAllActiveUsers();
+    const filteredRows = rows.filter((row) => {
+      return Object.keys(row).some(
+        (firstName) =>
+          row.firstName.toLowerCase().includes(searchedVal) ||
+          row.firstName.toUpperCase().includes(searchedVal)
       );
-    }
-    );
+    });
     setRows(filteredRows);
   };
 
   const cancelSearch = () => {
     setSearched("");
-    requestSearch(searched);
+    getAllActiveUsers();
+  };
+
+  const handleUpdateClick = async (row) => {
+    setData(row);
+    setOpenDialog(true);
   };
 
   return (
-    <>    
-    <div className='home'>
-    <div className='listContainer'>
-    <SearchBar
-          value={searched}
-        className='searchbar'
-          onChange={(searchVal) => requestSearch(searchVal)}
-          onCancelSearch={() => cancelSearch()}
-        />
+    <>
+      <div className="home">
+        <div className="listContainer">
+          <SearchBar
+            value={searched}
+            className="searchbar"
+            onChange={(searchVal) => requestSearch(searchVal)}
+            onCancelSearch={() => cancelSearch()}
+          />
 
-    <TableContainer component={Paper} 
-    className="usertable"
-    >
-      <Table sx={{ minWidth: 650 }}aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Agency Name</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow>
-              <TableCell>{row.id}</TableCell>
-              <TableCell className = "tableCell" key = {row.id}>{row.agencyname}</TableCell>
-              <TableCell>
-                    <button
-                      className="editbtn"
-                      title='Edit'
-                    >
-                      <EditOutlined />
-                    </button>
-                    <button
-                      className="deletebtn"
-                      title="Delete"
-                    >
-                      <DeleteOutlined />
-                    </button>
-                  </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-    </div>
+          <TableContainer component={Paper} className="usertable">
+            {isLoading && <ActivityLoader />}
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Verify</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {!isLoading &&
+                  rows.map((row) => (
+                    <TableRow key={row.id}>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell className="tableCell">
+                        {row.firstName}
+                      </TableCell>
+                      <TableCell className="tableCell">
+                        {row.lastName}
+                      </TableCell>
+                      <TableCell className="tableCell">
+                        {row.isVerified === "email" ? (
+                          <VerifiedIcon sx={{ color: "#fb9e00" }} />
+                        ) : (
+                          <CancelIcon sx={{ color: "red" }} />
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        <button
+                          className="editbtn"
+                          title="Edit"
+                          onClick={() => handleUpdateClick(row)}
+                        >
+                          <EditOutlined />
+                        </button>
+                        <button className="deletebtn" title="Delete">
+                          <DeleteOutlined />
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {openDialog && (
+            <Dialog
+              setOpenDialog={setOpenDialog}
+              getRecord={getAllActiveUsers}
+              dialogData={data}
+            />
+          )}
+        </div>
+      </div>
     </>
   );
 };
 
-export default AgencyDetail; 
+export default AgencyDetail;
